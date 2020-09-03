c2be7f93daf57c9cd81b48b6761705d0ad9d4b5d
saidorif <sayyid2112@gmail.com>
2020-09-03 11:53:09 +0500

    passport

---
+++ app/Passport.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Passport extends Model
+{
+    protected $fillable = ['region_from_id','region_to_id','area_from_id','area_to_id','name'];
+
+    public function regionFrom()
+    {
+        return $this->belongsTo(\App\Region::class,'region_from_id');
+    }
+
+    public function regionTo()
+    {
+        return $this->belongsTo(\App\Region::class,'region_to_id');
+    }
+
+    public function areaFrom()
+    {
+        return $this->belongsTo(\App\Area::class,'area_from_id');
+    }
+
+    public function areaTo()
+    {
+        return $this->belongsTo(\App\Area::class,'area_to_id');
+    }
+}
