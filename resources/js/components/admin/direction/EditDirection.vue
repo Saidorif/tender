dc9026d871de0a2d93bb8b7dbf67e28c6efb6f09
DevJavoxir <Java_0111_java@mail.ru>
2020-09-15 18:35:00 +0500

    edit_der

--- resources/js/components/admin/direction/EditDirection.vue
+++ resources/js/components/admin/direction/EditDirection.vue
@@ -50,16 +50,16 @@
             </div>
             <div class="form-group col-md-3">
               <label for="region_id">Area from</label>
-              <!-- <select
+              <select
                 class="form-control input_style"
-                v-model="form.area_from.area_id"
-                :class="isRequired(form.area_from.area_id) ? 'isRequired' : ''"
+                v-model="form.region_from.area_id"
+                :class="isRequired(form.region_from.area_id) ? 'isRequired' : ''"
                 placeholder="Area"
                 @change="selectArea('region_from')"
               >
                 <option value selected disabled>choose option</option>
                 <option :value="item.id" v-for="(item,index) in areaFrom">{{item.name}}</option>
-              </select> -->
+              </select>
             </div>
             <div class="form-group col-md-3">
               <label for="region_id">Station from</label>
@@ -136,7 +136,7 @@
             </div>
             <div class="form-group col-md-3">
               <label for="seria">direction Year</label>
-              <date-picker 
+              <date-picker
                 lang="ru" 
                 type="year"
                 v-model="form.year" 
@@ -163,17 +163,20 @@
             </div>
           </div>
         </form>
+        <PassportTab />
       </div>
     </div>
   </div>
 </template>
 <script>
 import DatePicker from "vue2-datepicker";
+import PassportTab from "../steppassport/PassportTab";
 import { mapGetters, mapActions } from "vuex";
-import 'vue2-datepicker/index.css';
+import "vue2-datepicker/index.css";
 export default {
   components: {
     DatePicker,
+    PassportTab
   },
   data() {
     return {
@@ -195,10 +198,10 @@ export default {
         distance: "",
         type_id: "",
       },
-      areaFrom:[],
-      areaTo:[],
-      stationFrom:[],
-      stationTo:[],
+      areaFrom: [],
+      areaTo: [],
+      stationFrom: [],
+      stationTo: [],
       requiredInput: false,
     };
   },
@@ -206,21 +209,22 @@ export default {
     await this.actionRegionList();
     await this.actionTypeofdirectionList();
     await this.actionEditDirection(this.$route.params.directionId);
-    // this.areaFrom = this.getAreaList
-    // // this.form = this.getDirection
-    this.form.pass_number = this.getDirection.pass_number
-    this.form.region_from.region_id = this.getDirection.region_from_id
-    this.form.region_from.area_id = this.getDirection.area_from_id
-    this.form.region_from.station_id  = this.getDirection.station_from_id
-    this.form.region_to.region_id = this.getDirection.region_to_id
-    this.form.region_to.area_id = this.getDirection.area_to_id
-    this.form.region_to.station_id  = this.getDirection.station_to_id
-    this.form.year  = this.getDirection.year
-    this.form.from_where  = this.getDirection.from_where
-    this.form.seasonal  = this.getDirection.seasonal
-    this.form.distance  = this.getDirection.distance
-    this.form.type_id  = this.getDirection.type_id
-    console.log(this.getDirection)
+    this.form.pass_number = this.getDirection.pass_number;
+    this.form.region_from.region_id = this.getDirection.region_from_id;
+    this.form.region_from.area_id = this.getDirection.area_from_id;
+    this.form.region_from.station_id = this.getDirection.station_from_id;
+    this.form.region_to.region_id = this.getDirection.region_to_id;
+    this.form.region_to.area_id = this.getDirection.area_to_id;
+    this.form.region_to.station_id = this.getDirection.station_to_id;
+    this.form.year = this.getDirection.year.toString();
+    this.form.from_where = this.getDirection.from_where;
+    this.form.seasonal = this.getDirection.seasonal;
+    this.form.distance = this.getDirection.distance;
+    this.form.type_id = this.getDirection.type_id;
+    this.areaFrom = this.getDirection.region_from_with.area;
+    this.areaTo = this.getDirection.region_to_with.area;
+    this.stationFrom = this.getDirection.area_from_with.station;
+    this.stationTo = this.getDirection.area_to_with.station;
   },
   methods: {
     ...mapActions("region", ["actionRegionList"]),
@@ -236,45 +240,44 @@ export default {
         this.form.pass_number != "" &&
         this.form.year != "" &&
         this.form.distance != "" &&
-        this.form.type_id != ""  &&
-        this.form.region_from_id != ""  &&
-        this.form.area_from_id != ""  &&
-        this.form.station_from_id != ""  &&
-        this.form.region_to_id != ""  &&
-        this.form.area_to_id != ""  &&
+        this.form.type_id != "" &&
+        this.form.region_from_id != "" &&
+        this.form.area_from_id != "" &&
+        this.form.station_from_id != "" &&
+        this.form.region_to_id != "" &&
+        this.form.area_to_id != "" &&
         this.form.station_to_id != "" &&
         this.form.from_where != "" &&
-        this.form.seasonal != "" 
+        this.form.seasonal != ""
       ) {
-		await this.actionAddDirection(this.form)
-        console.log(this.getMassage)
-		if(this.getMassage.success){
-			toast.fire({
-				type: "success",
-				icon: "success",
-				title: this.getMassage.message
-			 });
-			this.$router.push(`/crm/direction/${this.getMassage.id}`);
-		}else{
-			toast.fire({
-				type: "error",
-				icon: "error",
-				title: 'error whoops'
-			 });
-		}
-      this.requiredInput = false;
+        await this.actionAddDirection(this.form);
+        if (this.getMassage.success) {
+          toast.fire({
+            type: "success",
+            icon: "success",
+            title: this.getMassage.message,
+          });
+          this.$router.push(`/crm/direction/${this.getMassage.id}`);
+        } else {
+          toast.fire({
+            type: "error",
+            icon: "error",
+            title: "error whoops",
+          });
+        }
+        this.requiredInput = false;
       } else {
         this.requiredInput = true;
       }
     },
     async selectRegion(input) {
       await this.actionAreaByRegion({ region_id: this.form[input].region_id });
-      if(input == 'region_from'){
-        this.areaFrom = this.getAreaList
-        this.form.area_from_id = '' 
-      }else if(input == 'region_to'){
-        this.areaTo = this.getAreaList
-        this.form.area_to_id = '' 
+      if (input == "region_from") {
+        this.areaFrom = this.getAreaList;
+        this.form.area_from_id = "";
+      } else if (input == "region_to") {
+        this.areaTo = this.getAreaList;
+        this.form.area_to_id = "";
       }
     },
     async selectArea(input) {
@@ -282,12 +285,12 @@ export default {
         region_id: this.form[input].region_id,
         area_id: this.form[input].area_id,
       });
-      if(input == 'region_from'){
-        this.stationFrom = this.getStationsList
-        this.form.station_from_id = '' 
-      }else if(input == 'region_to'){
-        this.stationTo = this.getStationsList
-        this.form.station_to_id = '' 
+      if (input == "region_from") {
+        this.stationFrom = this.getStationsList;
+        this.form.station_from_id = "";
+      } else if (input == "region_to") {
+        this.stationTo = this.getStationsList;
+        this.form.station_to_id = "";
       }
     },
   },
@@ -297,6 +300,95 @@ export default {
     ...mapGetters("typeofdirection", ["getTypeofdirectionList"]),
     ...mapGetters("station", ["getStationsList"]),
     ...mapGetters("direction", ["getDirection"]),
+    destinations() {
+      let from = null;
+      let to = null;
+      let itemsFrom = [];
+      let itemsTo = [];
+      let arr = [null, null];
+      if (this.form.region_from.region_id && this.form.region_to.region_id) {
+        if (this.form.region_from.region_id == this.form.region_to.region_id) {
+          // If region_from 'id' is equal to region_to 'id'
+          if (this.form.region_from.area_id == this.form.region_to.area_id) {
+            // FROM
+            itemsFrom = this.form.region_from.station_id
+              ? this.stationFrom
+              : this.areaFrom;
+            from = this.form.region_from.station_id
+              ? this.form.region_from.station_id
+              : this.form.region_from.area_id;
+            itemsFrom.forEach((item) => {
+              if (item.id == from) {
+                arr[0] = item;
+              }
+            });
+            // TO
+            itemsTo = this.form.region_to.station_id
+              ? this.stationTo
+              : this.areaTo;
+            to = this.form.region_to.station_id
+              ? this.form.region_to.station_id
+              : this.form.region_to.area_id;
+            itemsTo.forEach((item) => {
+              if (item.id == to) {
+                arr[1] = item;
+              }
+            });
+          } else {
+            // FROM
+            itemsFrom = this.form.region_from.area_id
+              ? this.areaFrom
+              : this.getRegionList;
+            from = this.form.region_from.area_id
+              ? this.form.region_from.area_id
+              : this.form.region_from.region_id;
+            itemsFrom.forEach((item) => {
+              if (item.id == from) {
+                arr[0] = item;
+              }
+            });
+            // TO
+            itemsTo = this.form.region_to.area_id
+              ? this.areaTo
+              : this.getRegionList;
+            to = this.form.region_to.area_id
+              ? this.form.region_to.area_id
+              : this.form.region_to.region_id;
+            itemsTo.forEach((item) => {
+              if (item.id == to) {
+                arr[1] = item;
+              }
+            });
+          }
+        } else {
+          // FROM
+          itemsFrom = this.form.region_from.area_id
+            ? this.areaFrom
+            : this.getRegionList;
+          from = this.form.region_from.area_id
+            ? this.form.region_from.area_id
+            : this.form.region_from.region_id;
+          itemsFrom.forEach((item) => {
+            if (item.id == from) {
+              arr[0] = item;
+            }
+          });
+          // TO
+          itemsTo = this.form.region_to.area_id
+            ? this.areaTo
+            : this.getRegionList;
+          to = this.form.region_to.area_id
+            ? this.form.region_to.area_id
+            : this.form.region_to.region_id;
+          itemsTo.forEach((item) => {
+            if (item.id == to) {
+              arr[1] = item;
+            }
+          });
+        }
+        return arr;
+      }
+    },
   },
 };
 </script>
