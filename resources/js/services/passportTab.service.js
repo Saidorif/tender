588b997c67bb859c5f680308d800feefbf17ea7d
DevJavoxir <Java_0111_java@mail.ru>
2020-09-17 17:31:51 +0500

    id

--- resources/js/services/passportTab.service.js
+++ resources/js/services/passportTab.service.js
@@ -2,7 +2,7 @@ import ApiService from './api.service'
 
 const PassportTabService = {
 	addTiming(data){
-		return ApiService.post(`/api/timing/store`,data)
+		return ApiService.post(`/api/timing/store/${data.timing[0].direction_id}`,data)
 	},
 };
 
