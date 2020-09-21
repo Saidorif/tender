6544387cff482e2e4683eb665a214ed206b44d88
DevJavoxir <Java_0111_java@mail.ru>
2020-09-21 17:52:42 +0500

    nu

---
+++ resources/js/components/admin/steppassport/Schedule.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent.enter="saveData" enctype="multipart/form-data" class="row tabRow">
+    <h2>Schedule</h2>
+  </form>
+</template>
+<script>
+import DatePicker from "vue2-datepicker";
+import { mapGetters, mapActions } from "vuex";
+export default {
+  props: ["titulData"],
+  components: {
+    DatePicker,
+  },
+  data() {
+    return {
+      form:{ },
+    };
+  },
+  async mounted() {
+  },
+  computed: {
+    ...mapGetters("direction", ["getDirection"]),
+  },
+  methods: {
+    ...mapActions("passportTab", ["actionAddTiming", "clearTimingTable"]),
+    async saveData() {},
+    isRequired(input) {
+      return this.requiredInput && input === "";
+    },
+  },
+};
+</script>
+<style scoped>
+.tabRow {
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+</style>
