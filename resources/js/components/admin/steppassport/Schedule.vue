22f161a7f53f6a1552b70fb485fb2f375436acb0
DevJavoxir <Java_0111_java@mail.ru>
2020-09-22 12:40:37 +0500

    schedule

--- resources/js/components/admin/steppassport/Schedule.vue
+++ resources/js/components/admin/steppassport/Schedule.vue
@@ -1,6 +1,72 @@
 <template>
   <form @submit.prevent.enter="saveData" enctype="multipart/form-data" class="row tabRow">
-    <h2>Schedule</h2>
+    <h2>{{titulData.type.type}} - {{titulData.pass_number}} - sonli "{{titulData.name}}"Avtobus yo'nalishi qatnov yo'li masofasini va xarakat vaqtini olchash qaydnomasi</h2>
+    <div class="col-md-4">
+      <p>
+        Qatnov yo'l masofasi
+        <b>{{titulData.timing_with[titulData.timing_with.length - 1].end_speedometer}} km</b>
+      </p>
+    </div>
+    <div class="col-md-4">
+      <p>Qatnovchi avtomobillar soni</p>
+    </div>
+    <div class="col-md-4">
+      <p>Yolkira xaqqi so'm</p>
+    </div>
+    <div class="table-responisve">
+      <table class="table table-bordered text-center table-hover table-striped">
+        <thead>
+          <tr>
+            <th scope="col" colspan="8">Toshkent tomonga</th>
+            <th scope="col" rowspan="3">Toxtash joylarining nomlari</th>
+            <th scope="col" colspan="8">Nukus tomonga</th>
+          </tr>
+          <tr>
+            <th scope="col" colspan="8">Qatnov raqamlari va astronomik vaqt (soat-min)</th>
+            <th scope="col" colspan="8">Qatnov raqamlari va astronomik vaqt (soat-min)</th>
+          </tr>
+          <tr>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+            <th  colspan="1">Kelish vaqti</th>
+            <th  colspan="1">Jonash vaqti</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(table,index) in titulData.timing_with">
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1">{{table.region_from.name}}</th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+            <th  colspan="1"></th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </form>
 </template>
 <script>
@@ -13,11 +79,10 @@ export default {
   },
   data() {
     return {
-      form:{ },
+      form: {},
     };
   },
-  async mounted() {
-  },
+  async mounted() {},
   computed: {
     ...mapGetters("direction", ["getDirection"]),
   },
@@ -35,5 +100,4 @@ export default {
   padding-left: 30px;
   padding-right: 30px;
 }
-
 </style>
