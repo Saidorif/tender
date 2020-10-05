06f364b59652021450aa1c87876d557c5887276c
Your Name <mirxan085@gmail.com>
2020-10-05 18:02:26 +0500

    all

--- resources/js/components/admin/tenderannounce/AddTenderAnnounce.vue
+++ resources/js/components/admin/tenderannounce/AddTenderAnnounce.vue
@@ -62,13 +62,22 @@
 							<span slot="noOptions">Cписок пустой</span>
 						</multiselect>	
 					  </div>
+					  <div class="form-group col-md-2" v-if="checked">
+					    <label for="checkedGrafik">График</label>
+					    <input 
+					    	type="checkbox" 
+					    	class="form-control input_style" 
+					    	v-model="checkedGrafik" 
+					    	id="checkedGrafik"
+				    	>	
+					  </div>
 					  <div class="form-group col-md-2 form_btn" v-if="checked">
-						<button type="button" class="btn btn-secondary">
+						<button type="button" class="btn btn-secondary" @click="addToAllItems">
 							<i class="fas fa-plus"></i>
 							Добавить
 						</button>
 				  	  </div>
-					  <div class="form-group col-lg-6 form_btn d-flex justify-content-end">
+					  <div class="form-group col-lg-4 form_btn d-flex justify-content-end">
 					  	<button type="submit" class="btn btn-primary btn_save_category">
 					  		<i class="fas fa-save"></i>
 						  	Сохранить
@@ -76,19 +85,93 @@
 				  	  </div>
 					</div>
 				</form>
+				<!-- From Name -->
+				<div>
+				  	<div class="table-responsive" v-if="fromItems.length">
+				  		<div class="d-flex justify-content-center">
+				  			<h4>{{fromName}}</h4>
+				  		</div>
+					  	<table class="table table-bordered table-hover">
+					  		<thead>
+					  			<tr>
+					  				<th>№</th>
+					  				<th v-for="(item,index) in fromFirstItems.reys_times" colspan="2">{{item.where.name}}</th>
+					  			</tr>
+					  		</thead>
+					  		<tbody>
+					  			<tr 
+					  				v-for="(items,index) in fromItems" 
+					  				@click.prevent="chooseFromItem(items,index)"
+					  				:class="activeClass(items) ? 'active' : ''"
+				  				>
+					  				<td>
+					  					<label>
+				  							{{index+1}}
+				  						</label>
+					  				</td>
+					  				<template v-for="(item,key) in items.reys_times">
+						  				<td>{{item.start}}</td>
+						  				<td>{{item.end}}</td>
+					  				</template>
+					  			</tr>
+					  		</tbody>
+					  	</table>
+				  	</div>
+				  	<!-- To Name -->
+				  	<div class="table-responsive" v-if="fromItems.length">
+				  		<div class="d-flex justify-content-center">
+				  			<h4>{{toName}}</h4>
+				  		</div>
+					  	<table class="table table-bordered table-hover">
+					  		<thead>
+					  			<tr>
+					  				<th>№</th>
+					  				<th v-for="(item,index) in  toFirstItems.reys_times" colspan="2">{{item.where.name}}</th>
+					  			</tr>
+					  		</thead>
+					  		<tbody>
+					  			<tr v-for="(items,index) in  toItems">
+					  				<td>
+					  					<label>
+				  							{{index+1}}
+				  						</label>
+					  				</td>
+					  				<template v-for="(item,key) in items.reys_times">
+						  				<td>{{item.start}}</td>
+						  				<td>{{item.end}}</td>
+					  				</template>
+					  			</tr>
+					  		</tbody>
+					  	</table>
+				  	</div>
+				</div>
+			  	<!-- All choosen tables -->
 			  	<div class="table-responsive">
-				  	<table class="table table-bordered">
-				  		<thead>
-				  			<tr>
-				  				<th>№</th>
-				  			</tr>
-				  		</thead>
-				  		<tbody>
-				  			<tr>
-				  				<td>data</td>
-				  			</tr>
-				  		</tbody>
-				  	</table>
+			  		<div class="d-flex justify-content-center">
+			  			<h4>Выбранные маршруты</h4>
+			  		</div>
+				  	<div class="choosenItemsTable">
+				  		<ul v-for="(item,index) in choosenFromItems">
+				  		    <li>
+				  		    	<h4>{{index+1}}</h4>
+				  		    	<button class="btn btn-outline-success" type="button" data-toggle="collapse" :data-target="'#collapseExample'+index" aria-expanded="false" :aria-controls="'collapseExample'+index">
+								    {{item.reys_times[0].where.name}}-{{item.where.name}} 1 ta reys
+							  	</button>
+							  	<div class="collapse" :id="'collapseExample'+index">
+								  <table class="table table-bordered table-hover">
+								  		<tbody>
+								  			<tr>
+								  				<template v-for="(val,key) in item.reys_times">
+									  				<td>{{val.start}}</td>
+									  				<td>{{val.end}}</td>
+								  				</template>
+								  			</tr>
+								  		</tbody>
+								  	</table>
+								</div>
+				  			</li>
+				  		</ul>
+				  	</div>
 			  	</div>
 		  	</div>
 	  	</div>
@@ -114,9 +197,19 @@
 				requiredInput:false,
 				direction_ids:{},
 				checked:false,
+				checkedGrafik:false,
 				isLoading:false,
-				items:[],
-				findList:[]
+				allItems:[],
+				fromName:'',
+				fromItems:[],
+				fromFirstItems:[],
+				choosenFromItems:[],
+				toName:'',
+				toFirstItems:[],
+				toItems:[],
+				choosenToItems:[],
+				findList:[],
+				tableItems:[],
 			}
 		},
 		computed:{
@@ -131,6 +224,20 @@
 					this.direction_ids={}
 					this.findList = []
 				}
+			},
+			direction_ids:{
+				handler(){
+					if (this.direction_ids != '' || this.direction_ids != null) {
+						this.checkedGrafik = false
+						this.fromName = ''
+						this.choosenFromItems = ''
+						this.fromItems = []
+						this.fromFirstItems = []
+						this.choosenFromItems = []
+						this.toFirstItems = []
+						this.toItems = []
+					}
+				}
 			}
 		},
 		mounted(){
@@ -142,13 +249,43 @@
 			...mapActions("passportTab", [
 		      "actionGetScheduleTable",
 		    ]),
-			removeDirection(value){
-				this.form.direction_ids = this.form.direction_ids.filter((item,index)=>{
-					if(item != value.id){
-						return item
-					}
-				})
-			},
+		    addToAllItems(){
+		    	if (this.checkedGrafik){
+		    		let data = {
+		    			direction_id:'',
+		    			reys_id:'',
+		    			status:'grafik', 
+		    		}
+		    	}else{
+		    		let data = {
+		    			direction_id:'',
+		    			reys_id:'',
+		    			status:'grafik', 
+		    		}
+		    	}
+		    },
+		    activeClass(item){
+		    	if (this.choosenFromItems.some(data => data.id === item.id)) {
+	    			return true
+		    	}else{
+		    		return false
+		    	}
+		    },
+		    chooseFromItem(value,index){
+		    	if (this.choosenFromItems.some(data => data.id === value.id)){
+			    	Vue.delete(this.choosenFromItems, index)
+		    	}else{
+			    	this.choosenFromItems.push(value)
+		    	}
+		    	console.log(this.choosenFromItems)
+		    },
+			// removeDirection(value){
+			// 	this.form.direction_ids = this.form.direction_ids.filter((item,index)=>{
+			// 		if(item != value.id){
+			// 			return item
+			// 		}
+			// 	})
+			// },
 			isRequired(input){
 	    		return this.requiredInput && input === '';
 		    },
@@ -163,11 +300,18 @@
 		      }
 		    },
 		    async dispatchAction(data){
-		      this.form.direction_ids.push(data.id)
-		      if (this.checked) {
+		      this.form.direction_ids.push(data.id);
+		      if (this.checked){
 			      await this.actionGetScheduleTable(data.id)
-			      this.items = this.getSchedule
-			      console.log(this.items)
+			      console.log(this.getSchedule)
+			      // From Items
+			      this.fromFirstItems = this.getSchedule.whereFrom[0];
+			      this.fromItems = this.getSchedule.whereFrom
+			      this.fromName = this.getSchedule ? this.getSchedule.whereFrom[0].reys_times[0].where.name : ''
+			      // To Items
+			      this.toFirstItems = this.getSchedule.whereTo[0]
+			      this.toItems = this.getSchedule.whereTo
+			      this.toName = this.getSchedule ? this.getSchedule.whereTo[0].reys_times[0].where.name : ''
 		      }
 		    },
 			async saveTender(){
@@ -183,3 +327,11 @@
 		}
 	}
 </script>
+<style scoped>
+	tr{
+		cursor:pointer !important;
+	}
+	tr.active{
+		background:#d6d6d6;
+	}
+</style>
