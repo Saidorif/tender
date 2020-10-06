4ec0ed3dbc3785e5a1c20470ce23d6b04cc9e7ba
Your Name <mirxan085@gmail.com>
2020-10-06 08:48:56 +0500

    as

--- resources/js/components/admin/tenderannounce/AddTenderAnnounce.vue
+++ resources/js/components/admin/tenderannounce/AddTenderAnnounce.vue
@@ -265,11 +265,11 @@
 		    	}
 		    },
 		    activeClass(item){
-		    	// if (this.choosenFromItems.some(data => data.id === item.id)){
-	    		// 	return true
-		    	// }else{
-		    	// 	return false
-		    	// }
+		    	if (this.choosenFromItems.some(data => data.id === item.id)){
+	    			return true
+		    	}else{
+		    		return false
+		    	}
 		    },
 		    chooseFromItem(value,index){
 		    	if (this.choosenFromItems.some(data => data.id === value.id)){
