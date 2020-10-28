e25dcce85584392f92ca51dfcc8294be0e3f23e1
saidorif <sayyid2112@gmail.com>
2020-10-28 10:34:08 +0500

    srv

--- app/Http/Controllers/DirectionController.php
+++ app/Http/Controllers/DirectionController.php
@@ -395,8 +395,8 @@ class DirectionController extends Controller
             'whereFrom' => [],
             'whereTo' => [],
         ];
-        $reysesFrom = Reys::where(['direction_id' => $id,'status' => 'active','type' => 'from'])->get();
-        $reysesTo   = Reys::where(['direction_id' => $id,'status' => 'active','type' => 'to'])->get();
+        $reysesFrom = Reys::where(['direction_id' => $id,'type' => 'from'])->get();
+        $reysesTo   = Reys::where(['direction_id' => $id,'type' => 'to'])->get();
 
         foreach ($reysesFrom as $key => $reys_from) {
             // if($reys->where_type == 'region'){
