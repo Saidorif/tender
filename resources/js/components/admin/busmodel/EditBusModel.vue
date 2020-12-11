b4ea521802dfde9969d2f12a33886a1494a03b2e
DevJavoxir <Java_0111_java@mail.ru>
2020-12-11 11:06:26 +0500

    java

--- resources/js/components/admin/busmodel/EditBusModel.vue
+++ resources/js/components/admin/busmodel/EditBusModel.vue
@@ -11,22 +11,35 @@
 		  	<div class="card-body">
 		  		<form @submit.prevent.enter="saveType" >
 					<div class="row">
-					  <div class="form-group col-md-9">
+                        <div class="form-group col-md-3">
+                            <label for="busmodel_id">Марка автобуса</label>
+                            <select
+                                class="form-control input_style"
+                                v-model="form.busbrand_id"
+                                :class="isRequired(form.busbrand_id) ? 'isRequired' : ''"
+                            >
+                                <option value="" selected disabled>  Выберите тип автобус!  </option>
+                                <option  :value="item.id" v-for="(item, index) in getBusBrandList">
+                                    {{ item.name }}
+                                </option>
+                            </select>
+                        </div>
+					  <div class="form-group col-md-3">
 					    <label for="name">Название</label>
-					    <input 
-					    	type="text" 
-					    	class="form-control input_style" 
-					    	id="name" 
+					    <input
+					    	type="text"
+					    	class="form-control input_style"
+					    	id="name"
 					    	placeholder="Название"
 					    	v-model="form.name"
-					    	:class="isRequired(form.name) ? 'isRequired' : ''"  
+					    	:class="isRequired(form.name) ? 'isRequired' : ''"
 				    	>
 					  </div>
 					  <div class="form-group col-lg-3 d-flex justify-content-end saveBtn">
 					  	<button type="submit" class="btn btn-primary btn_save_category">
 					  		<i class="fas fa-save"></i>
 						  	Сохранить
-						</button>	
+						</button>
 				  	  </div>
 					</div>
 				</form>
@@ -40,20 +53,24 @@
 		data(){
 			return{
 				form:{
-					name:'',
+                    name:'',
+                    busbrand_id: '',
 				},
 				requiredInput:false
 			}
 		},
 		computed:{
-			...mapGetters('busmodel',['getMassage','getBusmodel'])
+            ...mapGetters('busmodel',['getMassage','getBusmodel']),
+            ...mapGetters("busbrand", ["getBusBrandList"]),
 		},
 		async mounted(){
-			await this.actionEditBusmodel(this.$route.params.busmodelId)
+            await this.actionEditBusmodel(this.$route.params.busmodelId)
+            await this.actionBusBrandList();
 			this.form = this.getBusmodel
 		},
 		methods:{
-			...mapActions('busmodel',['actionEditBusmodel','actionUpdateBusmodel']),
+            ...mapActions('busmodel',['actionEditBusmodel','actionUpdateBusmodel']),
+            ...mapActions("busbrand", ["actionBusBrandList"]),
 			isRequired(input){
 	    		return this.requiredInput && input === '';
 		    },
