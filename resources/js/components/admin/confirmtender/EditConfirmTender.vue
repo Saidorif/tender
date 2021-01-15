5f254ea67468bd95a014a79a2216ebfc500b27e8
DevJavoxir <Java_0111_java@mail.ru>
2021-01-15 15:55:43 +0500

    tableedit

--- resources/js/components/admin/confirmtender/EditConfirmTender.vue
+++ resources/js/components/admin/confirmtender/EditConfirmTender.vue
@@ -156,48 +156,86 @@
                             <span>{{items.reysesFrom[0].where.name}} - {{items.reysesFrom[0].from.name}}</span>
                             </h3>
                             <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                <th>№</th>
-                                <th v-for="(item,index) in items.reysesFrom[0].reys_times" colspan="2">
-                                    {{item.where.name}}
-                                </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr
-                                v-for="(reys,key) in items.reysesFrom"
-                                :class="activeEditClass(reys)"
-                                >
-                                <td>{{key+1}}</td>
-                                <template v-for="(val,key) in reys.reys_times">
-                                    <td>{{val.start}}</td>
-                                    <td>{{val.end}}</td>
-                                </template>
-                                </tr>
-                            </tbody>
+                                <thead>
+                                    <tr>
+                                        <th scope="col" rowspan="5" style="text-align:center;">Qatnovlar</th>
+                                        <th scope="col" :colspan="items.reysesFrom[0].reys_times.length * 2" style="text-align:center;">
+                                            {{items.reysesFrom[0].where.name}} томондан
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th colspan="2" v-for="(item, index) in items.reysesFrom[0].reys_times" style="text-align:center;">
+                                            {{item.where.name}}
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <template v-for="(item, index) in items.reysesFrom[0].reys_times">
+                                            <th style="text-align:center;">Прибытие</th>
+                                            <th style="text-align:center;">Отправление</th>
+                                        </template>
+                                    </tr>
+                                </thead>
+                                <!-- <thead>
+                                    <tr>
+                                        <th>№</th>
+                                        <th v-for="(item,index) in items.reysesFrom[0].reys_times" colspan="2">
+                                            {{item.where.name}}
+                                        </th>
+                                    </tr>
+                                </thead> -->
+                                <tbody>
+                                    <tr
+                                    v-for="(reys,key) in items.reysesFrom"
+                                    v-if="showTabeleReys(reys, t_lots, items)"
+                                    >
+                                    <td style="text-align:center;">{{key+1}}</td>
+                                    <template v-for="(val,key) in reys.reys_times">
+                                        <td style="text-align:center;">{{val.start}}</td>
+                                        <td style="text-align:center;">{{val.end}}</td>
+                                    </template>
+                                    </tr>
+                                </tbody>
                             </table>
                             <h3>
                             <span>{{items.reysesTo[0].where.name}} - {{items.reysesTo[0].from.name}}</span>
                             </h3>
                             <table class="table table-bordered">
-                            <thead>
+                                <thead>
+                                    <tr>
+                                        <th scope="col" rowspan="5" style="text-align:center;">Qatnovlar</th>
+                                        <th scope="col" :colspan="items.reysesTo[0].reys_times.length * 2" style="text-align:center;">
+                                            {{items.reysesTo[0].where.name}} томондан
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th colspan="2" v-for="(item, index) in items.reysesTo[0].reys_times" style="text-align:center;">
+                                            {{item.where.name}}
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <template v-for="(item, index) in items.reysesTo[0].reys_times">
+                                            <th style="text-align:center;">Прибытие</th>
+                                            <th style="text-align:center;">Отправление</th>
+                                        </template>
+                                    </tr>
+                                </thead>
+                            <!-- <thead>
                                 <tr>
                                 <th>№</th>
                                 <th v-for="(item,index) in items.reysesTo[0].reys_times" colspan="2">
                                     {{item.where.name}}
                                 </th>
                                 </tr>
-                            </thead>
+                            </thead> -->
                             <tbody>
                                 <tr
                                 v-for="(reys,key) in items.reysesTo"
-                                :class="activeEditClass(reys)"
+                                v-if="showTabeleReys(reys, t_lots, items)"
                                 >
-                                <td>{{key+1}}</td>
+                                <td style="text-align:center;">{{key+1}}</td>
                                 <template v-for="(val,key) in reys.reys_times">
-                                    <td>{{val.start}}</td>
-                                    <td>{{val.end}}</td>
+                                    <td style="text-align:center;">{{val.start}}</td>
+                                    <td style="text-align:center;">{{val.end}}</td>
                                 </template>
                                 </tr>
                             </tbody>
@@ -403,6 +441,27 @@ export default {
     //   });
     //   return count;
     // },
+    showTabeleReys(item, elem, parentItem){
+        if(elem != undefined){
+            if(elem.reys_id.includes(item.id)){
+                parentItem['byreys'] = true
+                return true
+            }else{
+                return false
+            }
+        }
+		// if (item.status == 'active') {
+		// 	return 'edit-active'
+		// }else{
+		// 	return 'edit-pending'
+		// }
+		// let lot_list = lots.reys_id
+		// if (lot_list.length > 0) {
+		// 	if (lot_list.includes(id)) {
+		// 		return true
+		// 	}
+		// }
+    },
     getLengthReys(reys){
 		let count = 0;
 			reys.reysesFrom.forEach((item,index)=>{
