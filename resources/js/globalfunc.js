81ca1651cdbe540297b225290d562d5d4c3602b9
DevJavoxir <Java_0111_java@mail.ru>
2021-01-22 10:35:59 +0500

    id

--- resources/js/globalfunc.js
+++ resources/js/globalfunc.js
@@ -78,25 +78,26 @@ export const g = {
         let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Display the result in the element with id=id
-        document.getElementById(id).innerHTML = `<div class="d-flex justify-content-center">
-          <div class="time_counter">
-            <div>${days}</div>
-            <div>день</div>
-          </div>
-          <div class="time_counter">
-            <div>${hours}</div>
-            <div>час</div>
-          </div>
-          <div class="time_counter">
-            <div>${minutes}</div>
-            <div>минут</div>
-          </div>
-          <div class="time_counter">
-            <div>${seconds}</div>
-            <div>секунд</div>
-          </div>
-        </div>`;
-
+        if(document.getElementById(id)){
+            document.getElementById(id).innerHTML = `<div class="d-flex justify-content-center">
+                <div class="time_counter">
+                <div>${days}</div>
+                <div>день</div>
+                </div>
+                <div class="time_counter">
+                <div>${hours}</div>
+                <div>час</div>
+                </div>
+                <div class="time_counter">
+                <div>${minutes}</div>
+                <div>минут</div>
+                </div>
+                <div class="time_counter">
+                <div>${seconds}</div>
+                <div>секунд</div>
+                </div>
+            </div>`;
+        }
         // If the count down is finished, write some text
         if (distance < 0) {
           clearInterval(x);
