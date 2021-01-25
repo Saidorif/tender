80648e9912513368079bae404eed666960e5376a
Your Name <mirxan085@gmail.com>
2021-01-25 10:19:22 +0500

    all

--- resources/js/services/completedtender.service.js
+++ resources/js/services/completedtender.service.js
@@ -2,7 +2,7 @@ import ApiService from './api.service'
 
 const CompletedTendersSerivce = {
 	completedTendersList(data){
-		return ApiService.get(`/api/tender/completed-tenders?page=${data.page}`,data.items)
+		return ApiService.post(`/api/tender/completed-tenders?page=${data.page}`,data.items)
 	},
 	completedTendersShow(id){
 		return ApiService.get(`/api/tender/completed-tenders/${id}`)
