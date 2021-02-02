2313bc613c396cbdd7f5421f179c31a123738a1c
saidorif <sayyid2112@gmail.com>
2021-02-02 12:24:35 +0500

    srv

--- app/Http/Controllers/DirectionController.php
+++ app/Http/Controllers/DirectionController.php
@@ -358,12 +358,70 @@ class DirectionController extends Controller
         if(!$direction){
             return response()->json(['error' => true, 'message' => 'Направление не найден']);
         }
+        if($direction->type_id == 1){
+            $total_result = $this->cityTarif($direction);
+        }else{
+            $ptimings = $direction->timing->toArray();
+            $total_result = [];
+            $test = [];
+            foreach($direction->passport_tarif as $index => $passportTarif){
+                $summa = $passportTarif->summa;
+                $summa_bagaj = $passportTarif->summa_bagaj;
+                $result = [];
+                for ($i=0; $i < count($ptimings); $i++) {
+                    foreach ($ptimings as $key => $timing) {
+                        $result[$i][$key]['ddd'] = 0;
+                        if($key != 0 && $i != 0){
+                            $result[$i][$key]['ddd'] += round($timing['distance_between_station'],2);
+                            if($key == 1 ){
+                                $distance_test = $timing['distance_between_station'];
+                            }else{
+                                $distance_test = floatval($result[$i][$key - 1]['distance_test']) + $result[$i][$key]['ddd'];
+                            }
+                        }
+                        else{
+                            $distance_test = round($timing['distance_from_start_station'],2);
+                        }
+                        if($ptimings[$i]['whereForm']['name'] == $timing['whereTo']['name'] || $i > $key){
+                            $result[$i][$key]['from_name'] = '';
+                            $result[$i][$key]['to_name'] = '';
+                            $result[$i][$key]['distance'] = '';
+                            $result[$i][$key]['distance_test'] = '';
+                            $result[$i][$key]['summa'] = round($summa,2);
+                            $result[$i][$key]['summa_bagaj'] = round($summa_bagaj,2);
+                            $result[$i][$key]['tarif'] = '';
+                            $result[$i][$key]['tarif_bagaj'] = '';
+                        }else{
+                            $result[$i][$key]['from_name'] = $ptimings[$i]['whereForm']['name'];
+                            $result[$i][$key]['to_name'] = $timing['whereTo']['name'];
+                            $result[$i][$key]['distance_test'] = round($distance_test,2);
+                            $result[$i][$key]['distance'] = round($distance_test,2);
+                            $result[$i][$key]['summa'] = round($summa,2);
+                            $result[$i][$key]['summa_bagaj'] = round($summa_bagaj,2);
+                            $result[$i][$key]['tarif'] = round($result[$i][$key]['summa'] * $result[$i][$key]['distance_test'],2);
+                            $result[$i][$key]['tarif_bagaj'] = round($result[$i][$key]['summa_bagaj'] * $result[$i][$key]['distance_test'],2);
+                        }
+                    }
+                }
+                $total_result[] = [
+                    'items' => $result,
+                    'tarif' => $passportTarif
+                ];
+            }
+        }
+        return response()->json(['success' => true, 'result' => $total_result]);
+    }
+
+    //Shahar yonalishlari uchun tarif
+    public function cityTarif(Direction $direction)
+    {
         $ptimings = $direction->timing->toArray();
         $total_result = [];
         $test = [];
-        foreach($direction->passport_tarif as $index => $passportTarif){
-            $summa = $passportTarif->summa;
-            $summa_bagaj = $passportTarif->summa_bagaj;
+        foreach($direction->regionFrom->tarifcity as $index => $passportTarif){
+            $passportTarif = $passportTarif->toArray();
+            $summa = $passportTarif['tarif'];
+            $summa_bagaj = $passportTarif['tarif_bagaj'];
             $result = [];
             for ($i=0; $i < count($ptimings); $i++) {
                 foreach ($ptimings as $key => $timing) {
@@ -405,7 +463,7 @@ class DirectionController extends Controller
                 'tarif' => $passportTarif
             ];
         }
-        return response()->json(['success' => true, 'result' => $total_result]);
+        return $total_result;
     }
 
     //Store direction tarifs
