2b78bae20eca60f3483375b61b4ab63b12e8bc7c
saidorif <sayyid2112@gmail.com>
2021-02-10 16:45:30 +0500

    srv

--- app/Http/Controllers/TenderController.php
+++ app/Http/Controllers/TenderController.php
@@ -128,7 +128,7 @@ class TenderController extends Controller
                 if($the_direction->status == 'approved'){
                     $the_direction_err[] = 'Невозможно добавить... Направление уже используется';
                 }
-                if($the_direction->type_id == 1 && $the_direction->tarif == 0){
+                if($the_direction->type_id != 1 && $the_direction->tarif == 0){
                     $the_direction_err[] = 'Тариф направления равен нулю. Пожалуйста, заполните поле';
                 }
                 if(!$the_direction->requirement){
