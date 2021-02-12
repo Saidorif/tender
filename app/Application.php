c5fcff9fc809a96db3101c68b3331e81378008e3
saidorif <sayyid2112@gmail.com>
2021-02-12 15:01:16 +0500

    srv

--- app/Application.php
+++ app/Application.php
@@ -58,4 +58,9 @@ class Application extends Model
     {
         return $this->hasMany(\App\File::class,'app_id');
     }
+    
+    public function balls()
+    {
+        return $this->hasOne(\App\ApplicationBall::class,'app_id')->with('user');
+    }
 }
