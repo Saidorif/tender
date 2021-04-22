f8c05fa16a22149a126e097322e8e8fccdf179c6
saidorif <sayyid2112@gmail.com>
2021-04-22 14:49:43 +0500

    'srv'

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -114,7 +114,7 @@ class ContractController extends Controller
         if($request->hasFile('file')){
             $file = $request->file('file');
             $ext = $file->getClientOriginalExtension();
-            if($ext != 'pdf'){
+            if($ext != 'pdf' || $ext != 'PDF'){
                 return response()->json(['error' => true, 'message' => 'File must be pdf']);
             }
             $path = 'public/'.date('Y-m-d');
