05af103298114523bd73a78260540ac422d7e922
saidorif <sayyid2112@gmail.com>
2021-05-20 10:22:38 +0500

    'srv'

--- database/migrations/2021_02_12_123954_create_application_balls_table.php
+++ database/migrations/2021_02_12_123954_create_application_balls_table.php
@@ -48,6 +48,7 @@ class CreateApplicationBallsTable extends Migration
             $table->double('tadbirlar_rejasi_ball',15,2)->nullable();
             $table->double('total_ball',15,2)->nullable();
             $table->string('status')->default('active')->nullable();
+            $table->string('reys_status')->nullable();
             $table->timestamps();
         });
     }
